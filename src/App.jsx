222691f0980c9adb17af68922115e5aa65604e08
su1nta <sukanta.developer@gmail.com>
2025-11-10 09:52:07 +0530

    chore:pattern and gradient bg

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,14 @@ function App() {
     <>
       <div className="min-h-screen md:w-screen bg-neutral-950 relative overflow-hidden">
         <div
-          className="absolute inset-0 h-full w-full z-0"
+          className="absolute h-1/5 inset-0 z-0 mask-b-from-70% pointer-events-none"
           style={{
             backgroundImage: `
-              radial-gradient(120% 90% at 50% -30%, rgba(26,43,68,0.9) 0%, rgba(3,5,8,0.85) 60%, transparent 85%)
+              linear-gradient(to right, rgba(64,64,64,0.2) 1px, transparent 1px),
+              linear-gradient(to bottom, rgba(64,64,64,0.2) 1px, transparent 1px),
+              radial-gradient(140% 80% at 50% -10%, rgba(59,130,246,0.25) 0%, rgba(26,43,68,0.25) 60%, transparent 75%)
             `,
-            backgroundColor: "#020305"
+            backgroundSize: "88px 88px, 88px 88px, 100% 100%, 100% 100%",
           }}
         />
         <div className="relative z-10 mx-auto flex max-w-full md:max-w-4xl xl:max-w-7xl flex-col items-center justify-center">
